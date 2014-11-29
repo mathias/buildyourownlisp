19c5216cae1cb1367d5bf3c1e5c92d8440856b3d
Matt Gauger <matt.gauger@gmail.com>
2014-11-29 13:16:46 -0600

    Chapter 7: Evaluation

--- parsing.c
+++ parsing.c
@@ -31,6 +31,38 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  /* If tagged as number turn it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child. */
+  char *op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv) {
 
   /* Create some parsers */
@@ -64,19 +96,9 @@ int main(int argc, char** argv) {
     /* Attempt ot Parse the user input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      /* on success print the AST */
-      /*mpc_ast_print(r.output);*/
+      long result = eval(r.output);
 
-      mpc_ast_t* a = r.output;
-      printf("Tag: %s\n", a->tag);
-      printf("Contents: %s\n", a->contents);
-      printf("Number of children: %i\n", a->children_num);
-
-      /* Get first Child */
-      mpc_ast_t* c0 = a->children[0];
-      printf("First Child Tag: %s\n", c0->tag);
-      printf("First Child Contents: %s\n", c0->contents);
-      printf("First Child Number of children: %i\n", c0->children_num);
+      printf("%li\n", result);
 
       mpc_ast_delete(r.output);
     } else {
