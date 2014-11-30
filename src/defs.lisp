4a51925728792c4c538b54efe36cd896b10148d7
Matt Gauger <matt.gauger@gmail.com>
2014-11-30 16:17:20 -0600

    Chapter 13 Bonus Marks: Create a recursive nth function

--- src/defs.lisp
+++ src/defs.lisp
@@ -8,3 +8,8 @@
 
 (fun {first lst} {eval (head lst)})
 (fun {second lst} {eval (head (tail lst))})
+
+(fun {len l} { if (== l {}) {0} {+ 1 (len (tail l))} })
+(fun {reverse l} { if (== l {}) {{}} {join (reverse (tail l)) (head l)} })
+
+(fun {nth lst n} { if (== n 0) {eval (head lst)} {nth (tail lst) (- n 1)}})
