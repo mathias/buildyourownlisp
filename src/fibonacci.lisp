4cf537f1d9c80d0538bdef340a888cfe912a1849
Matt Gauger <matt.gauger@gmail.com>
2014-12-04 21:38:12 -0600

    Recursive fibonacci

--- src/fibonacci.lisp
+++ src/fibonacci.lisp
@@ -2,3 +2,10 @@
 
 (defun {fib-iter a b count} { if (== count 0) {b} {fib-iter (+ a b) a (- count 1)} })
 (defun {fib n} {fib-iter 1 0 n})
+
+(defun {fib-recur n}
+  {if (== n 0)
+      {0}
+      {if (== n 1) {1}
+                  {+ (fib (- n 1))
+                     (fib (- n 2))}}})
